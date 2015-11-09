bc380137febb8fe49da6a43694115993a4e48878
Nguyen Hoang Nam <Alan>
2015-11-09 19:10:45 +0800

    Add Cocoapods

--- NStackView/ViewController.swift
+++ NStackView/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bt_Add: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func do_AddMore(sender: AnyObject) {
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-NStackView/Pods-NStackView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NStackView : NSObject
+@end
+@implementation PodsDummy_Pods_NStackView
+@end
