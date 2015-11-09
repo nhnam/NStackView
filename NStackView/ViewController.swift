6d126854882963e9a74f215fc99d91b1d0543771
Nguyen Hoang Nam <Alan>
2015-11-09 22:42:19 +0800

    Done

--- NStackView/ViewController.swift
+++ NStackView/ViewController.swift
@@ -12,11 +12,11 @@ import SnapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bt_Add: UIButton!
-
     var nextY:Float!
     var totalH:Float!
     var lastView:UIView!
     
+    //------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,19 +28,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //------------------------------------------------------------
     
     @IBAction func do_AddMore(sender: AnyObject) {
         let v:UIView = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(v)
         v.snp_makeConstraints { (make) -> Void in
-            make.height.equalTo(50)
+            make.height.equalTo((Int)(arc4random_uniform(50)+50))
             make.leading.equalTo(self.view).offset(30)
             make.trailing.equalTo(self.view).offset(-30)
             make.topMargin.equalTo(lastView.snp_bottom).offset(20)
         }
-        v.backgroundColor = UIColor.brownColor()
+        v.backgroundColor = randomColor()
         lastView = v
+    }
+    
+    //------------------------------------------------------------
+    func randomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
     }
 }
 
